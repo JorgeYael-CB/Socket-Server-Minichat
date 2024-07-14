ec69098a84c30c8efba4e2767f7eea6aa48f8fb0
JorgeYael-CB <jorge.yael333@gmail.com>
2024-07-14 14:36:52 -0600

    Todos los clientes escuchan el evento

--- index.js
+++ index.js
@@ -16,15 +16,22 @@ io.on('connection', ( socket ) => {
   // console.log(`Nuevo cliente conectado!`);
   // console.log(socket.id);
 
-  socket.emit('mensaje-bienvenida', {
-    msg: 'Bienvenido al server',
-    fecha: new Date(),
-  });
+  // socket.emit('mensaje-bienvenida', {
+  //   msg: 'Bienvenido al server',
+  //   fecha: new Date(),
+  // });
 
-  socket.on('mensaje-client', ( data ) => {
-    console.log('El cliente emitio al servidor!')
-    console.log(data);
-  });
+  // socket.on('mensaje-client', ( data ) => {
+  //   console.log('El cliente emitio al servidor!')
+  //   console.log(data);
+  // });
+
+
+  socket.on('message-to-server', (data) => {
+    console.log(data)
+
+    io.emit('message-from-server', data);
+  })
 
 });
 
