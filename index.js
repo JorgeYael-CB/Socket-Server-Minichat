5d749b4075570425b897e667ce91a9ec12bbb0be
JorgeYael-CB <jorge.yael333@gmail.com>
2024-07-14 13:15:38 -0600

    Agregamos la coneccion con socket-io

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Servidor de express
-const app = require('express')();
+const express = require('express')
+const app = express()
 
 // Servidor de Sockets
 const server = require('http').createServer(app);
 
 // Configuracion de Socket-Server
 const io = require('socket.io')(server);
+
+// Desplegar el directorio publico
+app.use( express.static( __dirname + '/public' ) )
 
 
 io.on('connection', () => {
